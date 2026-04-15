d732acb775152d3a9a0d5fe2fbc5be98edead492
Jun-Young Lee <toti010@naver.com>
2026-04-15 16:56:52 -0400

    patch boxsize for isolated bc

--- patch/init/halo/condinit.f90
+++ patch/init/halo/condinit.f90
@@ -111,9 +111,9 @@ subroutine condinit(r,g,x,q,dx,nn)
 
   ! Halo parameters from namelist
   eps = halo_eps ! small like 10 pc
-  xc = halo_center(1)+r%boxlen/2
-  yc = halo_center(2)+r%boxlen/2
-  zc = halo_center(3)+r%boxlen/2
+  xc = halo_center(1)+r%box_size(1)/2
+  yc = halo_center(2)+r%box_size(2)/2
+  zc = halo_center(3)+r%box_size(3)/2
   v200 = v_200 ! in [km/s]
   r200 = v200/hsmall ! in [kpc]
   M200 = (r200*hsmall/1.63d-2)**3/hsmall ! in [Msun]
