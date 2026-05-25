3f13d4975762fc771cb02ab7017e023154e220b9
Jun-Young Lee <toti010@naver.com>
2026-05-25 18:01:07 -0400

    Revert "Merge branch 'develop' of https://bitbucket.org/rteyssie/mini-ramses into amr-merge"
    This reverts commit 544d26b8dcbf24634e563a4efa2cb6a5c6de7983, reversing
    changes made to e2c18d1da509342be5c4e9e12ff6433d9be04a03.

--- amr/movie.f90
+++ amr/movie.f90
@@ -747,7 +747,7 @@ subroutine project_point(xx,xcen,ycen,zcen,theta_cam,phi_cam,dist_cam, &
      ycentre = x2+ycen
      zcentre = x3+zcen
   endif
-#elif NDIM>1
+#else
   x1 = xx(1)-xcen
   x2 = xx(2)-ycen
   xtmp = cos(theta_cam)*x1+sin(theta_cam)*x2
