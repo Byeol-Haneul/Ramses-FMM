7c204b2989dccf971d684ac71c0adfcd8827e884
Jun-Young Lee <toti010@naver.com>
2026-05-25 19:39:34 -0400

    Wire GPU FMM AMR direct pass

--- poisson/fmm/fmm_fine_commons.f90
+++ poisson/fmm/fmm_fine_commons.f90
@@ -1,6 +1,6 @@
 module fmm_fine_commons
 #if defined(_CUDA) && defined(WITHOUTMPI)
-  use gpu_runner, only: gpu_fmm_downward, gpu_fmm_amr_intermediate
+  use gpu_runner, only: gpu_fmm_downward, gpu_fmm_amr_intermediate, gpu_fmm_amr_direct
 #endif
 contains
 ! ------------------------------------------------------------------------
@@ -988,7 +988,11 @@ recursive subroutine r_fmm_amr_direct(pst,downward_levels,input_size)
        !! HERE WE DO NEAR FIELD + MID FIELD TOGETHER WITH 6^n cells
        call fmm_combined_direct(pst%s,downward_levels%ilev,downward_levels%jlev)
      else if (downward_levels%ilev==downward_levels%jlev) then
+#if defined(_CUDA) && defined(WITHOUTMPI)
+       call gpu_fmm_amr_direct(pst%s, downward_levels%ilev)
+#else
        call fmm_amr_direct(pst%s,downward_levels%ilev,downward_levels%jlev)
+#endif
      else
        call fmm_amr_direct_taylor(pst%s,downward_levels%ilev,downward_levels%jlev, &
             use_merged=(downward_levels%mode == FMM_TREE_SOURCE_MERGED)) ! important to exclude nearest neighbors.
